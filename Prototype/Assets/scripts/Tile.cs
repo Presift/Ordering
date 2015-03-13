2f0722abeed249ee651b7490ef9625c8429a4771
presift <lauracalexander85@gmail.com>
2015-03-13 10:51:23 -0700

    first commit

---
+++ Prototype/Assets/scripts/Tile.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	TileHolder holderScript;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	//COLLISION
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		// if other is a tileHolder and is not already occupied
+		if (other.name.Contains ("Holder")) 
+		{
+			TileHolder holdScript = ( TileHolder )other.gameObject.GetComponent(typeof( TileHolder ));
+			if( !holdScript.occupied )
+			{
+				SetTargetAndHolderColor( true, holdScript );
+			}
+
+		}
+	}
+	
+	void OnTriggerExit2D( Collider2D other )
+	{
+		//if other is target && other is this object's target
+		if (other.gameObject == holderScript.gameObject) 
+		{
+			SetTargetAndHolderColor( false, null );
+		}
+	}
+
+	void OnTriggerStay2D( Collider2D other )
+	{
+		//if target is null and object is tileHolder
+		if (holderScript == null && other.name.Contains ("Holder")) 
+		{
+			TileHolder holdScript = ( TileHolder )other.gameObject.GetComponent(typeof( TileHolder ));
+			if( !holdScript.occupied )
+			{
+				SetTargetAndHolderColor( true, holdScript );
+			}
+
+		}
+	}
+
+	void SetTargetAndHolderColor( bool highlight, TileHolder holdScript )
+	{
+		if (holdScript != null) 
+		{
+			holderScript = holdScript;
+			holderScript.Highlight( highlight );
+		}
+		else
+		{
+			holderScript.Highlight( highlight );
+			holderScript = holdScript;
+		}
+
+
+
+
+	}
+}
---
+++ Prototype/Assets/scripts/TileHolder.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileHolder : MonoBehaviour {
+
+	public SpriteRenderer rend;
+	public bool occupied = false;  //already holding a tile
+	Color originalColor;
+	public Color highlightedColor;
+
+	void Awake(){
+		originalColor = rend.color;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Highlight( bool highlight )
+	{
+		if (highlight) 
+		{
+			rend.color = highlightedColor;
+
+		}
+		else
+		{
+			rend.color = originalColor;
+		}
+	
+	}
+	
+
+	public void SetOccupied( bool filled )
+	{
+		occupied = filled;
+	}
+
+
+
+}
